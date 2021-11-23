4ce4e48ae72d242ab257bc73396717216fa7ebfc
Benjamin Bolte <bbolte@tesla.com>
2021-11-22 20:40:21 -0800

    more minimal

--- example/example.cpp
+++ example/example.cpp
@@ -1,13 +1,11 @@
-#define EXPORT __attribute__((visibility("default")))
-
 #include <pybind11/pybind11.h>
 #include <torch/extension.h>
 #include <torch/script.h>
 
-EXPORT torch::jit::Module module_identity(const torch::jit::Module &mod) {
+torch::jit::Module module_identity(const torch::jit::Module &mod) {
   return mod;
 }
-EXPORT torch::Tensor tensor_identity(const torch::Tensor &tnsr) { return tnsr; }
+torch::Tensor tensor_identity(const torch::Tensor &tnsr) { return tnsr; }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("module_identity", &module_identity);
