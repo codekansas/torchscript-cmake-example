2310ee1a7d30594bf0bbce6859cf295590fbd489
Benjamin Bolte <bbolte@tesla.com>
2021-11-26 22:21:11 -0800

    remove lib prefix

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import torch
 import torch.utils.cpp_extension
 from torch import Tensor, nn
 
-import libcpp
+import cpp
 
 
 class TestModule(nn.Module):
@@ -14,8 +14,8 @@ def main() -> None:
     tensor = torch.randn(3, 4)
     module = torch.jit.script(TestModule())
 
-    ident_tensor = libcpp.tensor_identity(tensor)  # This passes
-    ident_module = libcpp.module_identity(module._c)  # This fails
+    ident_tensor = cpp.tensor_identity(tensor)  # This passes
+    ident_module = cpp.module_identity(module._c)  # This fails
 
     print("ident tensor:", ident_tensor)
     print("ident module:", ident_module)
